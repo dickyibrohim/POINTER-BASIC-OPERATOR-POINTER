9b0d69647845e0ad68bf004f5c136e81c1ab2a61
DICKY IBROHIM <dicky.director@gmail.com>
2019-06-15 00:35:49 +0700

    First Upload
    First Upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main(){
+    int *ptr;
+    int k;
+    k=7;
+    printf("Isi variabel k = %d",k);
+    printf("\nAlamat variabel k = %d",&k);
+    printf("\nAlamat variabel *ptr = %d",&ptr);
+    printf("\nIsi variabel *ptr = %d",ptr); // Yang salah
+
+    ptr=&k;
+    printf("\nAlamat variabel *ptr = %d",&ptr);
+    printf("\nIsi variabel *ptr = %d",ptr);
+    printf("\nIsi dari alamat %d = %d",ptr,*ptr);
+    printf("\n");
+}
